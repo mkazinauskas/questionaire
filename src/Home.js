2f40af6591f8b9e6534fd19280fb2c79d1721132
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2019-11-26 23:10:57 +0200

    Updates

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,13 @@ import { Header } from 'semantic-ui-react'
 function Home() {
     return (
         <div>
-        <Header as='h1'>Darbų saugos mokymo testai</Header>
-        <p>Šie testai yra skirti pasiruošti valstybinės darbo inspekcijos (VDI) atestavimams</p>
+            <Header as='h1'>Darbų saugos mokymo testai</Header>
+            <ul>
+                <li>Šie testai yra skirti pasiruošti valstybinės darbo inspekcijos (VDI) darbų saugos egzaminams</li>
+                <li>Už klausimų ir atsakymų teisingumą neatsakau.</li>
+                <li>Už sistemos veikimą neatsakau.</li>
+                <li>Šiaip tai iš is už nieką neatsakau.</li>
+            </ul>
         </div>
     )
 }
