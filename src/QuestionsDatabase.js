b52eba13d6fd481bbd2a3d296159a5afe21112d2
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2019-11-26 19:44:44 +0200

    More questions

--- src/QuestionsDatabase.js
+++ src/QuestionsDatabase.js
@@ -226,6 +226,227 @@ const QuestionsDatabase = [
                 ),
             ]
         ), 
+        new Question(
+            'Bendrosios gaisrinės saugos taisyklės nurodo, kad automatiniai įvažiavimo į teritoriją vartai, užkardai ir kiti įrenginiai:',
+            [
+                new Answer(
+                    'turi turėti rankinį valdymą, leidžiantį juos atidaryti bet kuriuo paros metu.',
+                    true
+                ),
+                new Answer(
+                    'turi atsidaryti automatiškai.',
+                    false
+                ),
+                new Answer(
+                    'turi būti saugūs naudoti.',
+                    false
+                ),
+            ]
+        ), 
+        new Question(
+            'Kas praneša Valstybinei darbo inspekcijai apie įtariamą lėtinę profesinę ligą?',
+            [
+                new Answer(
+                    'Gydytojas.',
+                    true
+                ),
+                new Answer(
+                    'Darbdavys.',
+                    false
+                ),
+                new Answer(
+                    'Darbuotojas.',
+                    false
+                ),
+            ]
+        ),    
+        new Question(
+            'Kas yra nelaimingas atsitikimas pakeliui į darbą ar iš darbo?',
+            [
+                new Answer(
+                    'Tai įvykis, įskaitant eismo įvykį, darbuotojui vykstant į darbą ar iš darbo darbuotojo darbo dienomis kelyje tarp darbo vietos ir gyvenamosios vietos.',
+                    true
+                ),
+                new Answer(
+                    'Tai įvykis, įskaitant eismo įvykį darbuotojui darbo metu vykstant darbo reikalais.',
+                    false
+                ),
+                new Answer(
+                    'Tai įvykis, įskaitant eismo įvykį darbuotojui vykstant į darbą ar iš darbo įmonės ar jos samdomu transportu.',
+                    false
+                ),
+            ]
+        ),     
+        new Question(
+            'Ką reiškia sąvoka „profesinė rizika"?',
+            [
+                new Answer(
+                    'Ūmus ar lėtinis darbuotojo sveikatos sutrikimas dėl kenksmingo ir (ar) pavojingo darbo aplinkos rizikos veiksnio.',
+                    false
+                ),
+                new Answer(
+                    'Traumos ar kitokio darbuotojo sveikatos pakenkimo galimybė dėl kenksmingo ir (ar) pavojingo darbo aplinkos rizikos veiksnio (veiksnių) poveikio.',
+                    true
+                ),
+                new Answer(
+                    'Darbe esantys rizikos veiksniai, sukeliantys pavojų darbuotojų saugai ir sveikatai dėl darbo vietose vykstančių procesų.',
+                    false
+                ),
+            ]
+        ),   
+        new Question(
+            'Jeigu darbai yra susiję su užteršimu, ar darbuotojams turi būti nemokamai duodamos asmens higienos priemonės (muilas, rankšluosčiai ir kita)?',
+            [
+                new Answer(
+                    'Ne.',
+                    false
+                ),
+                new Answer(
+                    'Taip.',
+                    true
+                ),
+                new Answer(
+                    'Teisės aktai nereglamentuoja.',
+                    false
+                ),
+            ]
+        ),     
+        new Question(
+            'Jei darbuotojas pasirodė darbe neblaivus, apsvaigęs nuo narkotinių ar toksinių medžiagų, darbdavys:',
+            [
+                new Answer(
+                    'tą dieną (pamainą) neleidžia darbuotojui dirbti ir nemoka jam darbo užmokesčio.',
+                    true
+                ),
+                new Answer(
+                    'nedelsiant skiria drausminę nuobaudą.',
+                    false
+                ),
+                new Answer(
+                    'per vieną darbo dieną informuoja apie tai Valstybinės darbo inspekcijos teritorinį skyrių.',
+                    false
+                ),
+            ]
+        ),        
+        new Question(
+            'Darbuotojų klausos medicininių patikrinimų tikslas yra:',
+            [
+                new Answer(
+                    'anksti diagnozuoti galimą klausos praradimą dėl triukšmo ir išsaugoti klausą',
+                    true
+                ),
+                new Answer(
+                    'patvirtinti darbuotojų profesines ligas',
+                    false
+                ),
+                new Answer(
+                    'išgydyti darbuotojus dėl klausos netekimo',
+                    false
+                ),
+            ]
+        ),     
+        new Question(
+            'Kokią teisę turi darbo inspektorius, jei darbdavio įgaliotas asmuo ar darbdaviui atstovaujantis asmuo atsisako pavojaus darbuotojams atvejais vykdyti reikalavimą sustabdyti darbus?',
+            [
+                new Answer(
+                    'Turi teisę organizuoti darbuotojų evakavimą iš pavojingų vietų ar zonų.',
+                    false
+                ),
+                new Answer(
+                    'Turi teisę kreiptis į policiją pagalbos, kad būtų įvykdytas reikalavimas sustabdyti darbus ir iš pavojingų darbo vietų ar zonų būtų išvesti darbuotojai.',
+                    true
+                ),
+                new Answer(
+                    'Turi teisę nedelsiant visais įmanomais būdais užtikrinti, kad neliktų pavojaus darbuotojams.',
+                    false
+                ),
+            ]
+        ),    
+        new Question(
+            'Kokią apsaugos nuo elektros kategoriją pakanka turėti atsakingam už elektros ūkį asmeniui, jei elektros įrenginių leista naudoti galia viršija 100 kW?',
+            [
+                new Answer(
+                    'PK apsaugos nuo elektros kategoriją.',
+                    false
+                ),
+                new Answer(
+                    'VK apsaugos nuo elektros kategoriją.',
+                    false
+                ),
+                new Answer(
+                    'AK apsaugos nuo elektros kategoriją.',
+                    true
+                ),
+            ]
+        ),  
+        new Question(
+            'Į ką darbuotojas turi teisę kreiptis, jeigu darbo vietoje nėra saugios ir (ar) sveikatai nekenksmingos darbo sąlygos?',
+            [
+                new Answer(
+                    'Į darbuotojų atstovą, padalinio vadovą ar kitą darbdavio įgaliotą asmenį, darbdaviui atstovaujantį asmenį, įmonės darbuotojų saugos ir sveikatos komitetą, Valstybinę darbo inspekciją ar kitas valstybės institucijas.',
+                    true
+                ),
+                new Answer(
+                    'Visais atvejais pirmiausiai į Valstybinę darbo inspekciją ar kitas valstybės institucijas.',
+                    false
+                ),
+                new Answer(
+                    'Į padalinio vadovą, darbdavio įgaliotą asmenį darbuotojų saugai ir sveikatai, darbdaviui atstovaujantį asmenį, Valstybinę darbo inspekciją.',
+                    false
+                ),
+            ]
+        ),   
+        new Question(
+            'Įvykis nepripažįstamas nelaimingu atsitikimu darbe:',
+            [
+                new Answer(
+                    'jei įvykis darbe įvyko darbo laiku, darbo vietoje ir/ar atliekant darbo funkcijas, pareigas, darbdavio pavedimą arba kitą veiklą, už kurią įstatymų nustatyta tvarka mokamas vidutinis darbo užmokestis.',
+                    false
+                ),
+                new Answer(
+                    'jei įvykis įvyko tarnybinių komandiruočių metu, vykdant darbdavio užduotį paskirties vietoje.',
+                    false
+                ),
+                new Answer(
+                    'Ištyrus įvykį darbe ir nustačius, kad darbuotojas patyrė žalą sveikatai ar mirė, kai prieš jį panaudotas smurtas, jeigu smurto aplinkybės ir motyvai nesusiję su darbu.',
+                    true
+                ),
+            ]
+        ),       
+        new Question(
+            'Kas įmonėje organizuoja darbuotojų atstovų saugai ir sveikatai rinkimus bei darbuotojų atstovų saugai ir sveikatai komiteto narių rinkimus?',
+            [
+                new Answer(
+                    'Darbuotojų kolektyvo susirinkimas.',
+                    false
+                ),
+                new Answer(
+                    'Darbdaviui atstovaujantis asmuo.',
+                    true
+                ),
+                new Answer(
+                    'Įmonės profesinė sąjunga, o jei profesinės sąjungos nėra - kiti darbuotojų atstovai įmonės darbuotojų kolektyvo susirinkime.',
+                    false
+                ),
+            ]
+        ),    
+        new Question(
+            'Įmonės atskiro struktūrinio padalinio, esančio kitoje negu įmonė teritorijoje ar vietovėje, vadovo, kuris darbdaviui atstovaujančio asmens pavedimu ir jo kontroliuojamas įgyvendina reikiamas priemones, kad tokiame įmonės padalinyje sudarytų saugias ir sveikatai nekenksmingas darbo sąlygas, žinios iš darbuotojų saugos ir sveikatos srities tikrinamos tokia pat tvarka kaip:',
+            [
+                new Answer(
+                    'darbuotojų atstovo.',
+                    false
+                ),
+                new Answer(
+                    'darbuotojų saugos ir sveikatos specialisto.',
+                    false
+                ),
+                new Answer(
+                    'darbdaviui atstovaujančio asmens.',
+                    true
+                ),
+            ]
+        ),                      
     ]
     )
 ];
